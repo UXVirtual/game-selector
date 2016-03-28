d9e3382234766da0f197ffa18bd8cf1e9fe078cf
Michael Andrew <michael@gladeye.co.nz>
2016-03-28 16:09:24 +1300

    Now loading randomized game list

--- js/main.js
+++ js/main.js
@@ -1,15 +1,81 @@
-var div = document.createElement("div");
-div.className = "output";
-document.body.appendChild(div);
-
-var spinTwoThree = new SpinTwoThree(document.querySelector("#spin-two-three"));
-spinTwoThree.setCallbackStart(onSpinStart);
-spinTwoThree.setCallbackComplete(onSpinComplete);
+var $div, $slices, $spinTwoThree, $wheel, spinTwoThree, $triggerSpinBtn;
 
 function onSpinStart(e) {
-    div.innerHTML = "";
+
+    console.log('Spin started',e);
+
+    $div.html = $('');
 }
 
 function onSpinComplete(e) {
-    div.innerHTML = e.slots[0].getSlice(0).getAttribute("data-name");
+    console.log('Spin complete',e);
+    //$div.html = e.slots[0].getSlice(0).getAttribute("data-name");
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function loadData(){
+
+    $slices.html('');
+    shuffle(games);
+    renderGames();
+
+}
+
+function renderGames(){
+    var count = 0;
+    $.each(games,function(key,value){
+        $slices.append($('<div class="slice" data-name="'+value.id+'"><img src="'+value.header+'" /></div>'));
+        count++;
+    });
+    $wheel.css('height',count*215);
+}
+
+function onTriggerSpin(e){
+    e.preventDefault();
+    loadData();
+    //spinTwoThree.shuffle();
+    spinTwoThree.spin();
+
+}
+
+$(document).ready(function(){
+
+    $triggerSpinBtn = $('#trigger-spin-btn');
+
+    //console.log($triggerSpinBtn)
+
+    $div = $('<div class="output"></div>');
+    $spinTwoThree = $('#spin-two-three');
+    $wheel = $spinTwoThree.find('.wheel');
+    $slices = $spinTwoThree.find('.wheel > .slices');
+    $slices.html('');
+
+    $('body').append($div);
+
+    spinTwoThree = new SpinTwoThree($("#spin-two-three")[0]);
+    spinTwoThree.setCallbackStart(onSpinStart);
+    spinTwoThree.setCallbackComplete(onSpinComplete);
+
+    $triggerSpinBtn.bind('click',onTriggerSpin);
+
+    loadData();
+
+});
